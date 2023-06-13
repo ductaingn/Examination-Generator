d59cce541e46feff4230d7c64a85a14c39e37b03
R4C00n <superc0605@gmail.com>
2023-06-13 08:19:54 +0700

    Backup Database + Update QuestionChoice

--- src/Controllers/GUI32ChoiceController.java
+++ src/Controllers/GUI32ChoiceController.java
@@ -1,5 +1,37 @@
 package Controllers;
 
-public class GUI32ChoiceController {
+import Models.QuestionChoice;
+import com.sun.javafx.binding.StringFormatter;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GUI32ChoiceController implements Initializable {
+    @FXML
+    private ComboBox<String> gradeComboBox;
+    @FXML
+    private ImageView imageView;
+    @FXML
+    private TextArea textArea;
+
+    public void getGradeComboBox(){
+        QuestionChoice questionChoice = new QuestionChoice();
+        ObservableList<String> grades = FXCollections.observableArrayList();
+        for(int i=0;i<questionChoice.listGrade.size();i++){
+            String gradeString=String.format("%.5f",questionChoice.listGrade.get(i));
+            grades.add(gradeString+"%");
+        }
+        gradeComboBox.setItems(grades);
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        getGradeComboBox();
+    }
 }
--- src/Models/QuestionChoice.java
+++ src/Models/QuestionChoice.java
@@ -7,28 +7,19 @@ public class QuestionChoice {
     private String choiceText;
     private Integer choiceId;
     private String choiceImageSource;
-    public List<Double> choiceGrade;
+    private Double choiceGrade;
+    public List<Double> listGrade=new ArrayList<>();
 
-    QuestionChoice(){
+    public QuestionChoice(){
         double[] grade={100,90,500/6,80,75,70,400/6,60,50,40,200/6,30,25,20,100/6,100/7,100/8,100/9,10,5};
         for(int i=0;i<grade.length;i++){
-            choiceGrade.add(grade[i]);
+            listGrade.add(grade[i]);
         }
         for(int i=grade.length-1;i>=0;i--){
-            choiceGrade.add(-grade[i]);
+            listGrade.add(-grade[i]);
         }
     }
-    QuestionChoice(String choiceText){
-        this.choiceText=choiceText;
 
-        double[] grade={100,90,500/6,80,75,70,400/6,60,50,40,200/6,30,25,20,100/6,100/7,100/8,100/9,10,5};
-        for(int i=0;i<grade.length;i++){
-            choiceGrade.add(grade[i]);
-        }
-        for(int i=grade.length-1;i>=0;i--){
-            choiceGrade.add(-grade[i]);
-        }
-    }
     public String getChoiceText() {
         return choiceText;
     }
@@ -51,5 +42,13 @@ public class QuestionChoice {
 
     public void setChoiceImageSource(String choiceImageSource) {
         this.choiceImageSource = choiceImageSource;
+    }
+
+    public Double getChoiceGrade() {
+        return choiceGrade;
+    }
+
+    public void setChoiceGrade(Double choiceGrade) {
+        this.choiceGrade = choiceGrade;
     }
 }
