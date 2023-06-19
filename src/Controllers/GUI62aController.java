db801ca68bec183c75c430f3b317086b30cf22ab
DucChanHUST <kuntakufcvn@gmail.com>
2023-06-19 19:20:53 +0700

    shuffle GUI62

--- src/Controllers/GUI62aController.java
+++ src/Controllers/GUI62aController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.HBox;
@@ -15,11 +16,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.Vector;
+import java.util.*;
+
 public class GUI62aController extends GUI62aItemController implements Initializable{
+    @FXML
+    private CheckBox shuffle_ckb;
     @FXML
     private MenuItem aRandomQues_mit;
     @FXML
@@ -41,7 +42,6 @@ public class GUI62aController extends GUI62aItemController implements Initializa
     @FXML
     private Label title2_lbl;
     private static String nameData;
-    private static String idData;
     public void showGUI11() {
         Stage stage = (Stage)switch_lbl.getScene().getWindow();
         Model.getInstance().getViewFactory().closeStage(stage);
@@ -63,18 +63,10 @@ public class GUI62aController extends GUI62aItemController implements Initializa
         Model.getInstance().getViewFactory().showGUI65();
     }
    //show question list
-    public Connection getConnection() {
-        try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
     private List<QQuestion> qQuestionList(){
         Connection connection = getConnection();
-        String query = "SELECT name, text from ques_quiz, question " +
-                "WHERE ques_quiz.question_id = question.question_id and quiz_id = " + idData;
+        String query = "SELECT question.question_id, name, text from ques_quiz, question " +
+                "WHERE ques_quiz.question_id = question.question_id and quiz_id = " + quiz_id_data;
         List<QQuestion> list = new ArrayList<>();
         QQuestion qQuestion;
         try {
@@ -82,6 +74,7 @@ public class GUI62aController extends GUI62aItemController implements Initializa
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 qQuestion = new QQuestion();
+                qQuestion.setQuestion_id(resultSet.getInt("question_id"));
                 qQuestion.setName(resultSet.getString("name"));
                 qQuestion.setText(resultSet.getString("text"));
                 list.add(qQuestion);
@@ -90,7 +83,8 @@ public class GUI62aController extends GUI62aItemController implements Initializa
         return list;
     }
     public void getQuestionList(String quizId){
-        idData = quizId;
+        listQues.getChildren().clear();
+        quiz_id_data = quizId;
         List<QQuestion> qQuestionList = new ArrayList<>(qQuestionList());
         numOfQues_lbl.setText(""+qQuestionList.size());
         totalMark_lbl.setText(qQuestionList.size()+".00");
@@ -107,6 +101,25 @@ public class GUI62aController extends GUI62aItemController implements Initializa
             }
         }
     }
+    public void getQuestionList(String quizId, List shuffle_list){
+        listQues.getChildren().clear();
+        quiz_id_data = quizId;
+        List<QQuestion> shuffle = new ArrayList<>(shuffle_list);
+        numOfQues_lbl.setText(""+shuffle_list.size());
+        totalMark_lbl.setText(shuffle_list.size()+".00");
+        for (rank = 0; rank < shuffle_list.size(); rank++) {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/resources/Fxml/GUI62aItem.fxml"));
+            try {
+                HBox hBox = loader.load();
+                GUI62aItemController itemController62 = loader.getController();
+                itemController62.setData(shuffle.get(rank));
+                listQues.getChildren().add(hBox);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
     public void getTitle(String quizName) {
         nameData = quizName;
         title_lbl.setText("Editting quiz: " + quizName);
@@ -117,9 +130,9 @@ public class GUI62aController extends GUI62aItemController implements Initializa
         try {
             Statement statement = connection.createStatement();
             for (int i = 0; i < quesList.size(); i++) {
-                statement.executeUpdate("INSERT INTO ques_quiz(question_id, quiz_id) VALUES (" + quesList.get(i) + ", " + idData + ")");
+                statement.executeUpdate("INSERT INTO ques_quiz(question_id, quiz_id) VALUES (" + quesList.get(i) + ", " + quiz_id_data + ")");
             }
-            System.out.println(quesList.size()+" question added to quiz "+ idData);
+            System.out.println(quesList.size()+" question added to quiz " + quiz_id_data);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -127,10 +140,21 @@ public class GUI62aController extends GUI62aItemController implements Initializa
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         getTitle(nameData);
-        getQuestionList(idData);
+        getQuestionList(quiz_id_data);
         home_btn.setOnAction(event -> showGUI11());
         quiz_btn.setOnAction(event -> showGUI61());
         fromQuesBank_mit.setOnAction(event -> showGUI63a());
         aRandomQues_mit.setOnAction(event -> showGUI65());
+        shuffle_ckb.setOnAction(event -> {
+            if (shuffle_ckb.isSelected()) {
+                List shuffle_list = qQuestionList();
+                Collections.shuffle(shuffle_list);
+                getQuestionList(quiz_id_data, shuffle_list);
+                System.out.println("shuffle list");
+            } else {
+                getQuestionList(quiz_id_data);
+                System.out.println("non-shuffle list");
+            }
+        });
     }
 }
--- src/Controllers/GUI62aItemController.java
+++ src/Controllers/GUI62aItemController.java
@@ -3,26 +3,63 @@ package Controllers;
 import Models.QQuestion;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class GUI62aItemController implements Initializable {
+    @FXML
+    private Button delete_btn;
     @FXML
     private Label rank_lbl;
     @FXML
     private Label name_lb;
     @FXML
     private Label text_lbl;
+    @FXML
+    private Label quesID_lbl;
     public static int rank = 0;
+    public static String quiz_id_data;
     public void setData(QQuestion qQuestion){
         rank_lbl.setText(rank+1 +"");
         name_lb.setText(qQuestion.getName());
         text_lbl.setText(qQuestion.getText());
+        quesID_lbl.setText(qQuestion.getQuestion_id()+"");
     }
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public void deleteQuesFromQuiz(String ques_id){
+        Connection connection = getConnection();
+        try {
+            Statement statement = connection.createStatement();
+//            TODO
+//            query này cần update vì trong question sẽ có các choice,
+//            việc xóa question yêu cầu cần xóa các choice của question ấy trước
+            statement.executeUpdate("DELETE FROM ques_quiz WHERE question_id = " + ques_id + " and quiz_id = " + quiz_id_data + ";");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static String ques_id;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        delete_btn.setOnAction(event -> {
+            ques_id = quesID_lbl.getText();
+            deleteQuesFromQuiz(ques_id);
+            System.out.println("Delete question " + ques_id + " from quiz " + quiz_id_data);
+        });
     }
 }
