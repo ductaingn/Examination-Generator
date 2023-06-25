ffc2ecb068f972a1857f34f124ddb38ba5ec78d1
kien <Kien.ht215407@sis.hust.edu.vn>
2023-06-26 00:55:20 +0700

    update GUI74

---
+++ src/Controllers/GUI74Controller.java
@@ -0,0 +1,36 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class GUI74Controller {
+
+    @FXML
+    private Label completed_on;
+
+    @FXML
+    private Label grade;
+
+    @FXML
+    private Button home_btn;
+
+    @FXML
+    private Label marks;
+
+    @FXML
+    private Label started_on;
+
+    @FXML
+    private Label state;
+
+    @FXML
+    private Label switch_lbl;
+
+    @FXML
+    private Label timeTaken;
+
+    @FXML
+    private Label title2_lbl;
+
+}
