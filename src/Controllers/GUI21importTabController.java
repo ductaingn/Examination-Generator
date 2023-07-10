ef7f918a0cefa82050496964faca4c530f3eca3a
DucChanHUST <kuntakufcvn@gmail.com>
2023-07-10 19:29:34 +0700

    select category when import file Aiken

--- src/Controllers/GUI21importTabController.java
+++ src/Controllers/GUI21importTabController.java
@@ -1,10 +1,11 @@
 package Controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
 import javafx.scene.input.DragEvent;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Vector;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -30,6 +31,8 @@ import javafx.scene.control.Alert;
 
 public class GUI21importTabController implements Initializable {
     @FXML
+    private ComboBox<String> comboBox;
+    @FXML
     private ImageView image;
     @FXML 
     private Button button;
@@ -54,9 +57,46 @@ public class GUI21importTabController implements Initializable {
     }
 
     public Vector<Question> question_List = new Vector<>();
-
+    public void getComboBox() {
+        String queryCategoryName = ""+
+                "SELECT CONCAT( REPEAT(' ', COUNT(parent.name) - 1),' ' ,node.name,' (', " +
+                "(SELECT COUNT(question_id) FROM question " +
+                "WHERE question.category_id=node.category_id),') '  ) AS name " +
+                "FROM category AS node,category AS parent " +
+                "WHERE node.lft BETWEEN parent.lft AND parent.rgt " +
+                "GROUP BY node.category_id ORDER BY node.lft;";
+        Connection connection = getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(queryCategoryName);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            ObservableList<String> categoryName = FXCollections.observableArrayList();
+            while (resultSet.next()) {
+                String item = resultSet.getString("name");
+                categoryName.add(item);
+            }
+            comboBox.setItems(categoryName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private static int category_id;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        getComboBox();
+        comboBox.setOnAction(event -> {
+            String categoryName = comboBox.getValue().trim();
+            categoryName = categoryName.substring(0, categoryName.indexOf('(') - 1);
+            Connection connection = getConnection();
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement("" +
+                        "SELECT category_id FROM category WHERE name = '" + categoryName + "';");
+                ResultSet resultSet = preparedStatement.executeQuery();
+                resultSet.next();
+                category_id = resultSet.getInt("category_id");
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        });
         Label label = new Label();
         // Xử lý sự kiện khi bắt đầu kéo
         image.setOnDragDetected(event -> {
@@ -79,16 +119,12 @@ public class GUI21importTabController implements Initializable {
                 String filePath = dragboard.getFiles().get(0).getAbsolutePath();
                 label.setText("File path: " + filePath);
                 try {
+                    Image img = new Image("resources//Image//file.png");
+                    image.setImage(img);
                     if (checkAndAddAikenStructure(filePath)) {
-                    	Image img = new Image("resources//Image//file.png");
-                    	image.setImage(img);
                         success = true;
-                    }else {
-                    	Image img = new Image("resources//Image//file.png");
-                    	image.setImage(img);
                     }
                 } catch (IOException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
 
@@ -132,7 +168,7 @@ public class GUI21importTabController implements Initializable {
                         Connection connection = getConnection();
                         Statement statement = connection.createStatement();
                         for (int i = 0; i < question_List.size(); i++) {
-                            statement.executeUpdate("INSERT INTO question(category_id, mark,name, text) VALUES (1, 1, '"
+                            statement.executeUpdate("INSERT INTO question(category_id, mark,name, text) VALUES (" + category_id + ", 1, '"
                                     + question_List.elementAt(i).questionText +"','" + question_List.elementAt(i).questionText + "');");
                             statement.executeUpdate("set @id = LAST_INSERT_ID();");
                             for (int j = 0; j < question_List.elementAt(i).choiceList.size(); j++) {
@@ -182,7 +218,7 @@ public class GUI21importTabController implements Initializable {
                         Connection connection = getConnection();
                         Statement statement = connection.createStatement();
                         for (int i = 0; i < question_List.size(); i++) {
-                            statement.executeUpdate("INSERT INTO question(category_id, mark,name, text) VALUES (1, 1, '" + question_List.elementAt(i).questionText.substring(0,45) +"','" + question_List.elementAt(i).questionText + "');");
+                            statement.executeUpdate("INSERT INTO question(category_id, mark,name, text) VALUES (" + category_id + ", 1, '" + question_List.elementAt(i).questionText +"','" + question_List.elementAt(i).questionText + "');");
                             statement.executeUpdate("set @id = LAST_INSERT_ID();");
                             for (int j = 0; j < question_List.elementAt(i).choiceList.size(); j++) {
                                 if (question_List.elementAt(i).choiceList.elementAt(j).charAt(0) == question_List.elementAt(i).questionAnswer.charAt(8))
@@ -206,7 +242,6 @@ public class GUI21importTabController implements Initializable {
                     Model.getInstance().getViewFactory().showGUI21();
                 }
             } catch (IOException m) {
-                // TODO Auto-generated catch block
                 m.printStackTrace();
             }
         });
@@ -223,14 +258,17 @@ public class GUI21importTabController implements Initializable {
      
             while ((line = reader.readLine()) != null) {
                 if (count == 0) {
-                    // TODO
                 	num++;
                 	qs.questionText = line; 
                     count++;
                 } else if (count == 1) {
                     if (line.startsWith("A. ") || line.startsWith("B. ") || line.startsWith("C. ")
-                            || line.startsWith("D. ") || line.startsWith("E. ") || line.startsWith("F. ") || line.startsWith("G. ") || line.startsWith("H. ") || line.startsWith("I. ") || line.startsWith("K. ")) {
-                        // TODO
+                            || line.startsWith("D. ") || line.startsWith("E. ") || line.startsWith("F. ")
+                            || line.startsWith("G. ") || line.startsWith("H. ") || line.startsWith("I. ")
+                            || line.startsWith("K. ") || line.startsWith("A) ") || line.startsWith("B) ")
+                            || line.startsWith("C) ") || line.startsWith("D) ") || line.startsWith("E) ")
+                            || line.startsWith("F) ") || line.startsWith("G) ") || line.startsWith("H) ")
+                            || line.startsWith("I) ") || line.startsWith("K) ")){
                     	qs.choiceList.add(line);
                     	num++;
                     } else {
